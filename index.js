c620a48bf476f3295065fa878f3a42cdcb705817
Adnan Pedroso <adnan_silva54@hotmail.com>
2024-10-02 01:37:36 -0300

    webvitals

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) entry.target.classList.add("visible");
-    else entry.target.classList.remove("visible");
   });
 });
 
